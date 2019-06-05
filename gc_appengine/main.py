7934c3aaaedc6e1a7cd263929d286ce1a6ac2835
Prashant Abkari <prashant@aptira.com>
2019-06-05 13:03:27 +1000

    AddingGC App engine

---
+++ gc_appengine/main.py
@@ -0,0 +1,33 @@
+import logging
+
+from flask import Flask
+import json
+import requests
+
+
+app = Flask(__name__)
+
+
+def get_weather(api_key, location):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric".format(location, api_key)
+    r = requests.get(url)
+    return r.json()
+
+
+@app.route('/')
+def get_weather_details():
+    location = 'Melbourne'
+    api_key = '2fd76f4038d13d58e2b356cdfd7e8ed9'
+
+    weather = get_weather(api_key, location)
+
+    print(weather['main']['temp'])
+    print(weather)
+
+    return json.dumps(weather['main']['temp'])
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
